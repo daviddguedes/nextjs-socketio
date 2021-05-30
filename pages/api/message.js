b2b5eb727b72bf129d9bc0bb582b3becf03bcaf1
David Coelho <david.guedes@bolttech.io>
2021-05-30 17:05:16 -0300

    message json

--- pages/api/message.js
+++ pages/api/message.js
@@ -3,6 +3,6 @@ export default (req, res) => {
     console.log(res);
     const message = req.body.message;
     res?.socket?.server?.io?.emit("message", message);
-    res.status(201).json(message);
+    res.status(201).json({ message });
   }
 };
