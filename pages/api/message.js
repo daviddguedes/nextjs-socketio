8a688737aeddaede399d0cbdf8b8f5a583e66e69
David Coelho <david.guedes@bolttech.io>
2021-05-31 18:47:59 -0300

    remove nullables

--- pages/api/message.js
+++ pages/api/message.js
@@ -2,7 +2,7 @@ export default (req, res) => {
   if (req.method === "POST") {
     console.log('BODY...', req.body);
     const { message } = req.body;
-    res?.socket?.server?.io?.emit("message", message);
+    res.socket.server.io.emit("message", message);
     res.status(201).json({ message: JSON.stringify(req.body) });
   }
 };
