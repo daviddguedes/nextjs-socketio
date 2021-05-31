2e3ec9484a131b9f4d62c247074eb44bed30d4be
David Coelho <david.guedes@bolttech.io>
2021-05-31 18:52:34 -0300

    teste

--- pages/api/message.js
+++ pages/api/message.js
@@ -1,6 +1,6 @@
 export default (req, res) => {
   if (req.method === "POST") {
-    console.log('BODY...', req.body);
+    console.log('BODY...', res.socket.server.io);
     const { message } = req.body;
     res.socket.server.io.emit("message", message);
     res.status(201).json({ message: JSON.stringify(req.body) });
